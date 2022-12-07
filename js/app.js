4221d70b73cf34a4945262e2c361eef8ee1e4a94
joshres <97273313+joshres@users.noreply.github.com>
2022-12-06 19:04:44 -0800

    continued improvements for media queries

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 const dropContainer = document.querySelector('.drop-container');
 const dropMenuClick = document.querySelector('.drop-menu');
 const dropItem = document.querySelector('.dropdown-item');
+const overlay = document.querySelector(".overlay");
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");
+
 
 ////
 ///////////////////// Dropdown Menu /////////////////////
@@ -32,3 +36,24 @@ dropList.addEventListener("click" , e => {
     }
   }
   })
+
+////////// Open Modal /////////////
+
+gridContainer.addEventListener('click', e => {
+
+  // make sure the click is not on the gridContainer itself
+  if (e.target !== gridContainer) {
+
+      // select the card element based on its proximity to the actual element clicked
+      const card = e.target.closest(".card");
+      const index = card.getAttribute('data-index');
+
+      displayModal(index);
+  }
+});
+
+///////// Close Modal /////////
+
+modalClose.addEventListener('click', () => {
+  overlay.classList.add("hidden");
+});
