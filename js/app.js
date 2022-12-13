3207a44c17c5b8cc77b0e1eea0c9adbbc9cf2f07
joshres <97273313+joshres@users.noreply.github.com>
2022-12-12 19:53:03 -0800

    continued improvements all around

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const dropItem = document.querySelector('.dropdown-item');
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
+const mainName = document.querySelector(".name");
 
 
 ////
@@ -23,7 +24,7 @@ function changeMenu() {
 // target drop-menu and listen for click
 dropMenuClick.onclick = function() {dropMenu(), changeMenu()};
 
-// Close the dropdown menu if the user clicks outside of it
+// Close the dropdown menu if the user clicks the X
 const dropList = document.querySelector('.dropdown-list');
 dropList.addEventListener("click" , e => {
   if (!e.target.matches('.drop-item')) {
@@ -39,16 +40,26 @@ dropList.addEventListener("click" , e => {
 
 ////////// Open Modal /////////////
 
-gridContainer.addEventListener('click', e => {
+function displayModal(i) {
+  index = i;
+
+  const modalHTML = `
+    <img class="modal-image" src="images/josh-river.jpg">
+    <div class="mmodal-text-container">
+      <h2 class="modal-name">Josh Resnick</h2>
+      <p class="modal-about">I love things!</p>
+    </div>
+  `;
+
+  overlay.classList.remove("hidden");
+  modalContainer.innerHTML = modalHTML;
+};
+
+mainName.addEventListener('click', e => {
 
   // make sure the click is not on the gridContainer itself
-  if (e.target !== gridContainer) {
-
-      // select the card element based on its proximity to the actual element clicked
-      const card = e.target.closest(".card");
-      const index = card.getAttribute('data-index');
-
-      displayModal(index);
+  if (e.target === mainName) {
+    displayModal();
   }
 });
 
